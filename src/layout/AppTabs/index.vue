a03a6755c30b98805362bc8af5203eb1424dfa09
shiliangL <1462999406@qq.com>
2023-04-20 00:23:50 +0800

    2023年4月20日

--- src/layout/AppTabs/index.vue
+++ src/layout/AppTabs/index.vue
@@ -1,11 +1,35 @@
 <template>
     <div>
         AppTabs
+        <Headers></Headers>
+        <Body></Body>
     </div>
 </template>
 
 <script setup>
+import { useSlots, h } from "vue";
 
+const slots = useSlots();
+const props = defineProps(['modelValue','type'])
+
+const Body = () => {
+  return h("div",
+    {
+      class: { tab_body: true, "border-card": props.type === "border-card" },
+    },
+    {
+      default: () => [slots.default ? slots.default(): '什么鬼东西'],
+    }
+  );
+};
+
+const Headers = () => {
+  return h('div', {
+    value: props.modelValue,
+    type: props.type,
+    // onUpdateValue: getUpdateValue,
+  },'Headers-Headers');
+};
 </script>
 
 <style lang="scss" scoped>
